379d7375ab8020c4000f3fded6f8e372d2f64dd2
Amir Boussejra <amir@boussejra.com>
2022-11-30 15:41:10 +0100

    Setup on any pi

---
+++ README.md
@@ -0,0 +1,4 @@
+Run setup.sh
+update pip if needed
+Enable camera on pi if needed
+then do poetry run python server.py
---
+++ setup.sh
@@ -0,0 +1,18 @@
+sudo apt-get update -y
+sudo apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
+wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
+tar xf Python-3.7.3.tar.xz
+cd Python-3.7.3
+./configure
+make -j 4
+sudo make altinstall
+cd ..
+rm Python-3.7.3.tar.xz
+sudo apt-get --purge remove build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
+sudo apt-get autoremove -y
+sudo apt-get clean
+# Python installation done
+pip install poetry
+poetry install
+sudo apt install libgl1-mesa-glx
+wget https://github.com/Qengineering/TensorFlow_Lite_Pose_Jetson-Nano/raw/main/posenet_mobilenet_v1_100_257x257_multi_kpt_stripped.tflite --output-file -
